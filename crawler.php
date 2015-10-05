6dc7956bfe7692dfb4655773fd4bb3cf609edcba
root <felipemiramontesr@gmail.com>
2015-10-05 15:23:37 -0500

    Version 1.2 en la cual se realizan analisis de rangos cortos los cuales fueron usados para obtener el promedio de tiempo de ejecución de la aplicación

--- crawler.php
+++ crawler.php
@@ -16,21 +16,19 @@
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set("display_errors", "1");
 
-//Estrucutra de control que incremeta los valores de la sección a de una ip "a.b.c.d"
-for($a = 0; $a <= 1; $a++){
       //Estrucutra de control que incremeta los valores de la sección b de una ip "a.b.c.d"
-      for($b = 0; $b <= 1; $b++){
+      for($b = 0; $b <= 255; $b++){
             //Estrucutra de control que incremeta los valores de la sección c de una ip "a.b.c.d"
-           for($c = 0; $c <= 10; $c++){
+           for($c = 0; $c <= 255; $c++){
                  //Estrucutra de control que incremeta los valores de la sección d de una ip "a.b.c.d"
-                for($d = 0; $d <= 10; $d++){
+                for($d = 0; $d <= 255; $d++){
 
                      /*Se asigna la informacion detalla de la localización geografica de una IP a la 
  *variable $data por medio de la funcion GEOIP_RECORD_BY_NAME incluido en el paquete 
  *PECL GEOIP PHP
  */
 //$ip = $_POST["ip"];
-$aaa = strval($a);
+$aaa = "177";
 $bbb = strval($b);
 $ccc = strval($c);                     
 $ddd = strval($d);
@@ -64,31 +62,6 @@ if($data == false) {
                    }else{
 //Estructura de control que evalua si la variable $data se encuentra vacia o no 
 if ($data) {
-
-    /**
-     *Se asigna a la variable $code los datos extraidos de la página index del sitio
-     *por medio de la funcion FILE_GET_CONTENTS
-     */
-       $code = file_get_contents("http://www.saizac.com");
-       //$gestor = fopen("http://".$domain."/sites/default/settings.php", "r");
-       
-
-    /**
-     *Apertura o creación del nuevo archivo code.txt y escritura del codigo extraido
-     */
-
-      $file = fopen("code.txt", "w+")
-      or die("Error 01: Error al crear el nuevo archivo!");
-
-      /**
-      *Se escribe la información contenida en $code dentro del archivo code.txt
-      */
-
-      fwrite($file, $code);
-
-      //Cierre del archivo
-      fclose($file);            
-
     //Si la variable contiente información se imprime los datos en una tabla HTML
                     ?><table> 
                         <thead>
@@ -139,5 +112,5 @@ if ($data) {
                      }
                 }
            }   
-      }
+      
 ?>
--- spidy.php
+++ spidy.php
@@ -1,61 +1,76 @@
-<link href="style.css" rel="stylesheet" type="text/css">
 <?php
 /**
- *Programa que permite extrer el codigo del archivo index y las cabeceras 
+ *Programa que permite localizar geograficamente IPs en ciertos rangos...
  *PHP version 5
  * @category  PHP
  * @package   PHP_Spider
  * @author    Felipe de Jesus Miramontes Romero <felipemiraontesr@gmail.com>
  * @license   https://github.com/felipemiramontesr/spider GNU Licence
- * @version   SVN: $Id: crawler.php, v 1.0 2015-05-23 14:05:00 ff Exp $
+ * @version   SVN: $Id: crawler.php, v 1.2 2015-09-29 13:15:00 ff Exp $
  * @link      https://github.com/felipemiramontesr/spider
  */
-
 /**
-  *Apertura o creación del nuevo archivo code.txt y escritura del codigo extraido
-  */
-ini_set("max_execution_time", 1000000);
-echo "Working bitch!!!";
-      $file = fopen("data.txt", "w+")
-              or die("Error 01: Error al crear el nuevo archivo!");
-     
-//Reporte de errores: PHP5 notifica todos los errores menos los de tipo "Notice".
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set("display_errors", "1");
-
-      //Estrucutra de control que incremeta los valores de la sección b de una ip "a.b.c.d"
-      for($b = 0; $b <= 255; $b++){
-            //Estrucutra de control que incremeta los valores de la sección c de una ip "a.b.c.d"
-           for($c = 0; $c <= 255; $c++){
-                 //Estrucutra de control que incremeta los valores de la sección d de una ip "a.b.c.d"
-                for($d = 0; $d <= 255; $d++){
-
-                     /*Se asigna la informacion detalla de la localización geografica de una IP a la 
+*Se establece el valor de la directiva de configuración MAX_EXECUTION_TIME,
+*se realiza desde codigo para evitar modificar el archivo ini.php
+*/
+$time_start = microtime(true);
+ini_set('max_execution_time', 1000000);      
+/*
+*Configuración de reporte de errores: PHP5 notifica todos los contratiempos
+*excluyendo los de tipo "Notice".
+*/
+error_reporting(E_ALL ^ E_NOTICE); 
+/**
+*Se establece el valor de la directiva de configuración DISPLAY_ERRORS,
+*para ocultar los errores impresos en patalla
+*/
+ini_set('display_errors', '1');
+/*
+*Asignamos a la variable $file el archivo de nombre data2.txt, abierto y 
+*con capasidades de escritura (w+)
+*/
+$file = fopen('data2.txt', 'w+')
+        or die('Error 01: ¡Error al crear el nuevo archivo!');
+/*
+*Estrucutra de control que incremeta los valores de la sección d de una ip "a.b.c.d"
+*/
+for($b = 0; $b <= 255; $b++){
+                            for($c = 0; $c <= 255; $c++){
+                                                        for($d = 0; $d <= 255; $d++){
+/*Se asigna la informacion detalla de la localización geografica de una IP a la 
  *variable $data por medio de la funcion GEOIP_RECORD_BY_NAME incluido en el paquete 
  *PECL GEOIP PHP
  */
-//$ip = $_POST["ip"];
-$aaa = "177";
-$bbb = strval($b);
-$ccc = strval($c);                     
-$ddd = strval($d);
-
-$ip = $aaa.".".$bbb.".".$ccc.".".$ddd;                 
+$aip= '177';
+$bip = strval($b);
+$cip = strval($c);                     
+$dip = strval($d); 
+$ip = $aip.'.'.$bip.'.'.$cip.'.'.$dip;                
 $data = (geoip_record_by_name($ip));
-
+/*
+*Estructura de control que evalua si la variable $data se encuentra vacia o no 
+*/ 
 if($data == false) {
-                    
+/*
+*Si la variable $data contiene datos, se escribe la información en el archivo
+*data2.txt que anteriormente fue abierto 
+*/   
+                   echo 'Error 02: ¡No fue extraido ningun dato!';
                    }else{
+                        if($data){
+                                 fwrite($file, 'IP: '.$ip.' Localized in '.$data['country_name']."\r\n");
+                                 }
+                        }                                   
+                                                          
+                                                                                   } 
+                                                        }
+                            }                                                
+/*
+*Se cierra el archivo data2.txt 
+*/ 
+fclose($file);
 
-//Estructura de control que evalua si la variable $data se encuentra vacia o no 
-                  if ($data){
-                            fwrite($file, "IP: ".$ip." Localized in ".$data["country_name"]."\r\n");
-                            }                                    
-                         }              
-                     
-                     }
-                }
-           }   
-//Cierre del archivo
-fclose($file); 
+$time_end = microtime(true); 
+$time = $time_end - $time_start;
+echo 'Process execution time: '. $time.' Miliseconds';
 ?>
